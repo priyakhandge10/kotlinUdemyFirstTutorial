4f3bdd47271d70970e28a3cb8c167694b2a533c4
priyanka khandge <priyakhandge10@gmail.com>
2020-02-04 16:13:13 +0530

    oprator example

---
+++ src/KotlinOperator.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    var a = 10
+    var b = 20
+    var c = ((a + b) * (a - b)) / 2 //arithmetic operator
+    var d = a > b // comparison operator
+    var e = a < b
+    var f = a == b
+    var g = a++
+    var h = b--
+
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+
+}
