c4bdbd36099bcb8646dccf35b073cbc6b243c77e
priyanka khandge <priyakhandge10@gmail.com>
2020-02-04 15:00:48 +0530

    writing hello wird program

---
+++ src/HelloWord.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var v1=10 //var variable mutable
+    v1=20
+
+    val v2 = 26 //val variable read only
+
+    var abc=15 // type casting
+    val bcd:Long=abc.toLong()
+
+    println(v1)
+    println(v2)
+    println(abc)
+    println(bcd)
+    println("hello word")
+}
