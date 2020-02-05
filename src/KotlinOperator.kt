07f13f8d6a4d52524d26ed89e91de007312fe315
priyanka khandge <priyakhandge10@gmail.com>
2020-02-05 14:52:24 +0530

    ArrayList example

--- src/KotlinOperator.kt
+++ src/KotlinOperator.kt
@@ -4,7 +4,7 @@ fun main() {
     var b = 20
     var c = ((a + b) * (a - b)) / 2 //arithmetic operator
     var d = a > b // comparison operator
-    var e = a < b
+    var e = a < b       
     var f = a == b
     var g = a++
     var h = b--
---
+++ src/collections/ArraysLists.kt
@@ -0,0 +1,15 @@
+package collections
+
+fun main(args: Array<String>) {
+
+    val x = arrayOf("kotlin","tutorial","example")
+    val y = x.get(2)
+    //val z = x.set(1, 2)
+    val list = mutableListOf("priya","rahul", "shelke","1")
+    list.add("priyanka")
+   println(x[1])
+    println(y)
+    //println(z)
+    println(list)
+
+}
