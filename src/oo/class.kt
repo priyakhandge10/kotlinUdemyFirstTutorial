260743576d32d7d3dc625d3e06587dc2d1d3dbe7
priyanka khandge <priyakhandge10@gmail.com>
2020-02-06 12:45:41 +0530

    class example

---
+++ src/oo/class.kt
@@ -0,0 +1,13 @@
+package oo
+
+class Person
+{
+    var name: String = "Priyanka"
+    var age: Int = 26
+}
+
+fun main(args: Array<String>) {
+    val person = Person()
+    println(person.name)
+    println(person.age)
+}
